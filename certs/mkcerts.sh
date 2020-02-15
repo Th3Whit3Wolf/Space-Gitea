138f460981c0df2ae214e240d1cd133506f4e750
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2020-02-15 21:26:37 +0000

    Initial commit

---
+++ certs/mkcerts.sh
@@ -0,0 +1,4 @@
+openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
+openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+openssl req -x509 -in server.req -text -key server.key -out server.crt
+chmod 600 server.key
